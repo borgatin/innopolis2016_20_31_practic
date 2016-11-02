a822944c49fbc7adc7dc5ef9826c3313f167c203
a.borgatin <avborg90@yandex.ru>
2016-11-02 21:34:42 +0300

    sa

--- src/main/java/ru/innopolis/borgatin/client/controller/MainController.java
+++ src/main/java/ru/innopolis/borgatin/client/controller/MainController.java
@@ -14,12 +14,12 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(value = "/")
 public class MainController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/login")
+    @RequestMapping(/*method = RequestMethod.GET,*/ value = "/login")
     public String login(Model model){
         return "login";
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/")
+    @RequestMapping(/*method = RequestMethod.GET,*/ value = "/")
     public String start(Model model){
         return "welcome";
     }
